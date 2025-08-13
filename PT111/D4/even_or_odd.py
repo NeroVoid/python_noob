b0c567a57ebb7c179fbee7694822e886cf853e9e
Nero Void <8365640+NeroVoid@users.noreply.github.com>
2025-08-13 23:23:32 +0000

    Check even or odd

---
+++ PT111/D4/even_or_odd.py
@@ -0,0 +1,6 @@
+# This code checks if a number is even or odd.
+number = int(input("Enter an integer: "))
+if number % 2 == 0:
+    print("The number you entered is even.")
+else:
+    print("The number you entered is odd.")
