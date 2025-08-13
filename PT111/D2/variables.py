ca0b86ed0217bd53604f34e8a83a72166d5e7801
Nero Void <8365640+NeroVoid@users.noreply.github.com>
2025-08-13 21:22:55 +0000

    Practice declaring and using variables

---
+++ PT111/D2/variables.py
@@ -0,0 +1,19 @@
+# Declare variables and assign values
+name = "Alice"
+age = 25
+height = 1.68
+is_student = True
+
+# Print the variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+# Change the value of a variable
+age = age + 1 # Increment age by 1
+print("Age after one year:", age)
+
+# Calculating with variables
+year_of_birth = 2025 - age
+print("Year of Birth:", year_of_birth)  
