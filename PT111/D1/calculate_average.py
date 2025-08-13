815238f8ddaf2149666c032b4bc0e56f59a6378e
Nero Void <8365640+NeroVoid@users.noreply.github.com>
2025-08-13 21:12:13 +0000

    Add calculate_average.py to compute the average of three user-input numbers

---
+++ PT111/D1/calculate_average.py
@@ -0,0 +1,6 @@
+# This code calculates the average of three numbers input by the user.
+number_1 = float(input("Enter the first number: "))
+number_2 = float(input("Enter the second number: "))
+number_3 = float(input("Enter the third number: "))
+average = (number_1 + number_2 + number_3) / 3
+print("The average is:", average)
