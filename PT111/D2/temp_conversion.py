215e7ab9b4ff7b0a70a9ef7a2030cffbdeb2c599
Nero Void <8365640+NeroVoid@users.noreply.github.com>
2025-08-13 21:33:53 +0000

    Add code to convert Celsius to Fahrenheit

--- PT111/D2/temp_conversion.py
+++ PT111/D2/temp_conversion.py
@@ -1,3 +1,4 @@
-c=float(input("Enter temperature in Celsius: "))
-f=(c*9/5)+32
-print("Temperature in Fahrenheit:", f)
+# Convert from Celsius to Fahrenheit
+temp_c=float(input("Enter temperature in Celsius: "))
+temp_f=(temp_c*9/5)+32
+print("Temperature in Fahrenheit is:", temp_f)
