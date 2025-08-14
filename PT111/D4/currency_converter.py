2a498c50f7a95c4c122f40164225afd39bc1bbd2
Nero Void <8365640+NeroVoid@users.noreply.github.com>
2025-08-14 02:54:36 +0000

    Edit rounding number

--- PT111/D4/currency_converter.py
+++ PT111/D4/currency_converter.py
@@ -8,8 +8,8 @@ if conversion_type == '1':
     vnd_amount = round(usd_amount * 26000)  # Assuming 1 USD = 26000 VND
     print(usd_amount, "USD is equal to", vnd_amount, "VND.")
 elif conversion_type == '2':
-    vnd_amount = round(float(input("Enter the amount in VND: ")))
-    usd_amount = vnd_amount / 26000  # Assuming 1 USD = 26000 VND
+    vnd_amount = float(input("Enter the amount in VND: "))
+    usd_amount = round((vnd_amount / 26000),2)  # Assuming 1 USD = 26000 VND
     print(vnd_amount,"VND is equal to", usd_amount,"USD.")
 else:
     print("Invalid selection. Please enter 1 or 2.")
