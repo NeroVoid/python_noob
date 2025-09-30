d335f842b881cc175ec5b348090538774abde175
Nero Void <8365640+NeroVoid@users.noreply.github.com>
2025-09-30 13:29:13 +0000

    Create final assignment's folder

---
+++ PT111/D19/student_mgmt.py
@@ -0,0 +1,47 @@
+'''
+# Bài 1 — Ứng dụng Quản lý Sinh viên (CRUD cơ bản)
+
+## Mô tả
+
+Ứng dụng giúp quản lý danh sách sinh viên trong lớp. Dữ liệu mỗi sinh viên gồm:
+
+Mã số sinh viên (id)
+
+Họ tên (name)
+
+Tuổi (age)
+
+Ngành học (major)
+
+Danh sách được lưu trữ bằng list chứa dictionary.
+
+## Yêu cầu
+
+Viết menu chương trình với các chức năng:
+
+Thêm sinh viên
+
+Cập nhật thông tin sinh viên theo id
+
+Xoá sinh viên theo id
+
+Tìm kiếm sinh viên theo tên
+
+Hiển thị toàn bộ danh sách sinh viên
+
+Lưu danh sách vào file students.csv và có thể đọc lại dữ liệu từ file.
+
+Xử lý ngoại lệ khi:
+
+Nhập trùng id.
+
+Xoá hoặc sửa sinh viên không tồn tại.
+
+Gợi ý data mẫu (students.csv)
+```bash
+id,name,age,major
+1,Duong,21,IT
+2,Hoa,22,Economics
+3,Linh,20,Design
+```
+'''
---
+++ PT111/D19/text_analysis.py
@@ -0,0 +1,31 @@
+'''
+# Bài 2 — Phân tích Dữ liệu từ File Văn bản
+
+## Mô tả
+
+Bạn được cung cấp một file article.txt chứa một đoạn văn bản dài. Nhiệm vụ là phân tích để thống kê dữ liệu.
+
+## Yêu cầu
+
+Đọc nội dung từ file `article.txt`.
+
+Thống kê số dòng, số từ trong văn bản.
+
+Đếm số lần xuất hiện của từng từ (dùng dictionary).
+
+Ghi kết quả thống kê ra file report.txt theo định dạng:
+
+```bash
+Tổng số dòng: X
+Tổng số từ: Y
+Từ xuất hiện nhiều nhất: Z (N lần)
+```
+Xử lý ngoại lệ khi file không tồn tại.
+
+## File data mẫu (article.txt)
+```bash
+Python is a popular programming language.
+Python is easy to learn and powerful.
+Many developers love Python.
+```
+'''
